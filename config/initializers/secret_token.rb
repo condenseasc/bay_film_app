66287f8872277f657d2102680a569e3c60dfed3b
Leibniz <seth.neill@gmail.com>
2013-08-29 18:48:57 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BayFilmApp::Application.config.secret_key_base = 'ddaa046d58bbb44ad4aa794ccec27f83acc2eea35f68443395694ed80c9c857270f460de8f9ef49f405b3c23fe265907016f4623587901802066f62c4714511d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BayFilmApp::Application.config.session_store :cookie_store, key: '_bay_film_app_session'
