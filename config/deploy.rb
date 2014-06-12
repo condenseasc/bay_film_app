e2fc6cbe78a149bcc9cf792295a534551cd3169d
sethdn <seth.neill@gmail.com>
2014-06-12 15:23:54 -0700

    add unicorn to cap/rbenv settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,7 +39,7 @@ set :linked_files, %w{config/database.yml}
 set :rbenv_type, :user # or :system, depends on your rbenv setup
 set :rbenv_ruby, '2.1.2'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
-set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_map_bins, %w{rake gem bundle ruby rails unicorn}
 set :rbenv_roles, :all # default value
 
 
